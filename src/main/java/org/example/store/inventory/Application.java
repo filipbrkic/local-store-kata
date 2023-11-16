58cc6932c560bc9c0f7d30aec346d5d789e7c3c2
fbrkic <filip.brkic@barrage.net>
2023-11-16 14:18:51 +0100

    Update updateQuality - feature/RND-457

--- src/main/java/org/example/store/inventory/Application.java
+++ src/main/java/org/example/store/inventory/Application.java
@@ -10,55 +10,89 @@ public class Application {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Event Tickets")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("The Best Store in Town Award")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            updateItemQuality(items[i]);
+            updateSellIn(items[i]);
+            handleExpiredItem(items[i]);
+        }
+    }
 
-                    if (items[i].name.equals("Event Tickets")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+    private void updateItemQuality(Item item) {
+        if (isNormalItem(item)) {
+            decreaseQuality(item);
+        } else if (isAgedBrie(item) || isEventTicket(item)) {
+            increaseQuality(item);
+            if (isEventTicket(item)) {
+                handleEventTicketQuality(item);
             }
+        }
+    }
 
-            if (!items[i].name.equals("The Best Store in Town Award")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+    private void updateSellIn(Item item) {
+        if (!isTheBestStoreAward(item)) {
+            item.sellIn--;
+        }
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Event Tickets")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("The Best Store in Town Award")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+    private void handleExpiredItem(Item item) {
+        if (item.sellIn < 0) {
+            if (isNormalItem(item)) {
+                decreaseQuality(item);
+            } else if (isAgedBrie(item)) {
+                increaseQuality(item);
+            } else if (isEventTicket(item)) {
+                item.quality = 0;
+            } else if (isPerishablePork(item)) {
+                decreaseQuality(item, 4);
+                if (item.quality < 0) {
+                    item.quality = 0;
                 }
             }
         }
     }
-    
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void handleEventTicketQuality(Item item) {
+        if (item.sellIn < 11) {
+            increaseQuality(item);
+        }
+
+        if (item.sellIn < 6) {
+            increaseQuality(item);
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+
+    private void decreaseQuality(Item item, int amount) {
+        item.quality -= amount;
+    }
+
+    private boolean isNormalItem(Item item) {
+        return !isAgedBrie(item) && !isEventTicket(item) && !isTheBestStoreAward(item) && !isPerishablePork(item);
+    }
+
+    private boolean isAgedBrie(Item item) {
+        return "Aged Brie".equals(item.name);
+    }
+
+    private boolean isEventTicket(Item item) {
+        return "Event Tickets".equals(item.name);
+    }
+
+    private boolean isTheBestStoreAward(Item item) {
+        return "The Best Store in Town Award".equals(item.name);
+    }
+
+    private boolean isPerishablePork(Item item) {
+        return "Perishable Pork".equals(item.name);
+    }
 }
